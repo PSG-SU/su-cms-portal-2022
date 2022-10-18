4e891cd000245f69ab5bcb3ac80b470de1edcd84
tgashwinkumar <tgashwinkumar@gmail.com>
2022-10-18 21:20:07 +0530

    API config error resolved

--- frontend-sdk/src/API/config.js
+++ frontend-sdk/src/API/config.js
@@ -2,8 +2,8 @@ const BASE_URL = 'http://localhost:8080';
 const API_URL = `${BASE_URL}/api`;
 
 export const AUTH_URL = `${API_URL}/auth`;
-export const LOGIN_URL = `${AUTH_URL}login`;
-export const SIGNUP_URL = `${AUTH_URL}signup`;
+export const LOGIN_URL = `${AUTH_URL}/login`;
+export const SIGNUP_URL = `${AUTH_URL}/signup`;
 
 export const UPLOAD_URL = `${API_URL}/upload`;
 
