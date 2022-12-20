f70a476691aaef9454e40a077ad9755a9b17110d
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2022-12-21 00:55:09 +0530

    Upload Changes

--- frontend-sdk/src/pages/Club/Events/AddEvent.js
+++ frontend-sdk/src/pages/Club/Events/AddEvent.js
@@ -6,7 +6,7 @@ import FileUpload from "../../../components/FileUpload";
 import MultipleFiles from "../../../components/MultipleFiles";
 import Heading from "../../../components/Heading";
 import TextArea from "../../../components/TextArea";
-import { fetchGetApprovedorPublishedProposal, fetchUpdateProposal } from "../../../API/calls";
+import { fetchGetApprovedorPublishedProposal, fetchUpdateProposal, fetchUploadFile } from "../../../API/calls";
 import Dropdown from "../../../components/Dropdown";
 import toast from "react-hot-toast";
 import { EventContext } from ".";
@@ -19,6 +19,7 @@ const AddEvent = () => {
   const [eventName, setEventName] = useState("");
   const [desc, setDesc] = useState("");
   const [files, setFiles] = useState([]);
+  const [fileUrls, setFileUrls] = useState([]);
 
   useEffect(() => {
     console.log("Update State: ", updateState);
@@ -75,19 +76,49 @@ const AddEvent = () => {
     }
   }, [eventName])
 
+  const handleSingleUpload = (files, curr_no, total) => {
+    if (files.length <= 0) return;
+
+    const currentFile = files.pop();
+    toast.promise(fetchUploadFile(currentFile), {
+      loading: `Uploading... ${curr_no}/${total}`,
+      success: (res) => {
+        let tempFileUrls = fileUrls;
+        tempFileUrls.push(res.data.url);
+        setFileUrls(tempFileUrls);
+        handleSingleUpload(files, curr_no + 1, total);
+        return `Uploaded ${curr_no}/${total}`;
+      },
+      error: "Error Occured",
+    });
+  };
+
+  const handleUpload = async () => {
+    if (eventName.length <= 0) return toast.error("Event name required.");
+    if (files.length <= 0) return toast.error("Files required for upload.");
+    await handleSingleUpload(files, 1, files.length);
+  };
+
   const handlePublishEvent = () => {
-    const postBody = {
-      description: desc,
-      images: files,
-      status: "published",
-    }
-    toast.promise(fetchUpdateProposal(postBody, ID)
-      .then((res) => {
-        window.location.reload();
-      }), {
-      loading: "Updating...",
-      success: "Updated Successfully",
-      error: (err) => `Error: ${err.response.data.error}`,
+    toast.promise(handleUpload(), {
+      loading: "Uploading...",
+      success: (res) => {
+        const postBody = {
+          description: desc,
+          images: fileUrls,
+          status: "published",
+        }
+        toast.promise(fetchUpdateProposal(postBody, ID)
+          .then((res) => {
+            window.location.reload();
+          }), {
+          loading: "Updating...",
+          success: "Updated Successfully",
+          error: (err) => `Error: ${err.response.data.error}`,
+        });
+        return "Upload completed";
+      },
+      error: "Error Occured",
     });
   }
 
@@ -124,7 +155,7 @@ const AddEvent = () => {
         </div>
         <div className="flex items-center space-x-4 mt-8 w-1/2">
           <Button className="w-3/4" text="Publish" handleClick={handlePublishEvent} />
-          { (Object.keys(updateState).length > 0 || eventName ) && <Button className="w-3/4" text="Cancel" handleClick={handleCancel} />}
+          {(Object.keys(updateState).length > 0 || eventName) && <Button className="w-3/4" text="Cancel" handleClick={handleCancel} />}
         </div>
       </div>
     </section>
